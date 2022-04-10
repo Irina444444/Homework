de06cd64e817a7ce45567ea7fb7def603a00570c
Irina444444 <staat4444@gmail.com>
2022-04-10 23:38:18 +0300

    Add Task3

---
+++ Task3 EvenNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Add number: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2 == 0)
+
+{
+    Console.WriteLine("Even number");
+}
+
+else
+
+{
+    Console.WriteLine("Odd number");
+}
