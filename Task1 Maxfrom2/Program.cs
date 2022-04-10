9937a9fcf074c103bdebf1e6e346cccc99c97a6e
Irina444444 <staat4444@gmail.com>
2022-04-10 22:04:57 +0300

    Task1

---
+++ Task1 Maxfrom2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Insert first figure for comparison: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Insert second figure for comparison: ");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number1);
+}
+
+if (number1 < number2)
+
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number2);
+}
+
+if (number1 == number2)
+
+{
+    Console.WriteLine("Both figures are equal");
+}
