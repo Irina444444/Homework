6dd9569ac168fe96a2e5f729b199a483999973eb
Irina444444 <staat4444@gmail.com>
2022-04-10 23:03:22 +0300

    Task2

---
+++ Task2 Maxfrom3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Insert three figure for comparison: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+int number2 = Convert.ToInt32(Console.ReadLine()); 
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if ((number1 > number2) && (number1 > number3))
+
+{   
+    Console.WriteLine("Max from three: ");
+    Console.WriteLine(number1);
+}
+
+if ((number2 > number1) && (number2 > number3))
+
+{
+    Console.WriteLine("Max from three: ");
+    Console.WriteLine(number2);
+}
+if ((number3 > number1) && (number3 > number2))
+
+{
+    Console.WriteLine("Max from three: ");
+    Console.WriteLine(number3);
+}
