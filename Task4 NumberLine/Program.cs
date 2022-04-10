b4c3a28aa36c4f14b0434104d05910abac4d7471
Irina444444 <staat4444@gmail.com>
2022-04-11 01:23:26 +0300

    Task4

---
+++ Task4 NumberLine/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Insert figure");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Even figures:");
+
+for (int i = 1; (i <= number) && (i > 0); i++)
+
+if (i % 2 == 0)
+{
+    Console.WriteLine( + i);
+}
+
+for (int i = number; i < 0; i++)
+
+if (i % 2 == 0)
+
+Console.WriteLine( + i);
