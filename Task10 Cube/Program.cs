8d04e3d103efb2465eb0b5f86a50016afbfec86b
Irina444444 <staat4444@gmail.com>
2022-04-19 16:39:26 +0300

    Task10

---
+++ Task10 Cube/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Enter any number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; (i <= number) & (i > 0); i++)
+
+{
+                Console.WriteLine(Math.Pow(i, 3));
+}
+for (int i = number; i < 0; i++)
+
+{
+       Console.Write(Math.Pow(i, 3));
+}
+if (number == 0)
+{
+    Console.WriteLine("0");
+}
