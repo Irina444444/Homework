9b078126254716718e0d50fc5e38c7dbfb53831d
Irina444444 <staat4444@gmail.com>
2022-04-19 15:38:01 +0300

    Task9

---
+++ Task9 distance3D/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Insert x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine(result);
